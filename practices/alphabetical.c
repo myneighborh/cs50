e20c1c21d1a4c818f86886890cc13dae97e9098b
myneighborh <jeaha03@gmail.com>
2025-04-24 20:41:04 +0900

    Add alphabeticla.c

---
+++ practices/alphabetical.c
@@ -0,0 +1,38 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    // Get user's input
+    if (argc != 2)
+    {
+        printf("Please provide a word.\n");
+            return 1;
+    }
+
+    string text = argv[1];
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (!isalpha(text[i]))
+        {
+            printf("This is not a letter.\n");
+            return 2;
+        }
+    }
+
+    // Iterate through each element in the string
+    for (int i = 1; i < strlen(text); i++)
+    {
+        // check if it is alphabetical
+        if (text[i] < text [i - 1])
+        {
+            printf("No\n");
+            return 0;
+        }
+    }
+    printf("Yes\n");
+    return 0;
+}
